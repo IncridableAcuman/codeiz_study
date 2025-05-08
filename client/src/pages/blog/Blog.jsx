65f5d0cfe77292755ac1f140d0e12d875b4dae14
Izzatbek <abdusharipovizzat03@gmail.com>
2025-05-08 10:41:53 +0500

    e-learning

--- client/src/pages/blog/Blog.jsx
+++ client/src/pages/blog/Blog.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import Navbar from '../../components/Navbar'
 import BlogHeader from './BlogHeader'
+import BlogList from './BlogList'
 
 const Blog = () => {
   return (
     <>
     <div className="">
         <Navbar/>
+        {/* blog header */}
         <div className="paddingCnt bg-sky-400 min-h-[70vh]">
             <BlogHeader/>
+        </div>
+        {/* blog list */}
+        <div className="paddingCnt">
+          <h2 className='pb-5 pl-5 lg:pl-10 text-lg font-bold'>Reading blog list</h2>
+          <div className="">
+            <BlogList/>
+          </div>
         </div>
     </div>
     </>
--- client/src/pages/blog/BlogList.jsx
+++ client/src/pages/blog/BlogList.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
 const BlogList = () => {
+  const data=[
+    {img:"./a.png",title:"UX/UI"},
+    {img:"./a.png",title:"React"},
+    {img:"./a.png",title:"Java"},
+    {img:"./a.png",title:"JavaScript"},
+  ]
   return (
-    <div>BlogList</div>
+    <>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 sm:gap-6 lg:gap-8 px-4 sm:px-6 lg:px-8">
+      {
+        data.map((item,index)=>(
+          <div className="relative pb-2" key={index}>
+            <img src={item.img} alt={item.title} className=' w-full h-auto object-cover rounded-2xl' />
+              <button className=' absolute bottom-4 left-4 right-4 sm:left-6 sm:right-6 lg:left-8 lg:right-8 p-2 rounded-full sm:p-2.5 text-sm sm:text-base text-center truncate bg-gray-300 opacity-95'>{item.title}</button>
+          </div>
+        ))
+      }
+    </div>
+    </>
   )
 }
 
