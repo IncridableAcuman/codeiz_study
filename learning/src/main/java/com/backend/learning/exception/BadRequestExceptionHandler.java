c7d184d870feabdbf412ac3b3dfe35791e5b11b8
Izzatbek <abdusharipovizzat03@gmail.com>
2025-05-13 17:49:17 +0500

    e-learning

---
+++ learning/src/main/java/com/backend/learning/exception/BadRequestExceptionHandler.java
@@ -0,0 +1,5 @@
+package com.backend.learning.exception;
+
+public class BadRequestExceptionHandler {
+    
+}
--- learning/src/main/java/com/backend/learning/exception/GlobalExceptionHandler.java
+++ learning/src/main/java/com/backend/learning/exception/GlobalExceptionHandler.java
@@ -1,5 +1,10 @@
 package com.backend.learning.exception;
 
-public class GlobalExceptionHandler {
-    
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
 }
