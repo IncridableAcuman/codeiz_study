abb934084fd5f40a3db47fc231cd1ba3d2f62250
Izzatbek <abdusharipovizzat03@gmail.com>
2025-05-14 12:35:35 +0500

    e-learning

--- learning/src/main/java/com/backend/learning/LearningApplication.java
+++ learning/src/main/java/com/backend/learning/LearningApplication.java
@@ -3,10 +3,16 @@ package com.backend.learning;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class LearningApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv=Dotenv.load();
+		dotenv.entries().forEach(entry->{
+			System.setProperty(entry.getKey(), entry.getValue());
+		});
 		SpringApplication.run(LearningApplication.class, args);
 	}
 
